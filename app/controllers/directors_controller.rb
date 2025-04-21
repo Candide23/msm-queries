38fb8201926886c83aa4eea9d600f5870a684564
Candide Mboungou-Kimpolo <89795037+Candide23@users.noreply.github.com>
2025-04-21 18:28:22 +0000

    assignement done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -11,5 +11,25 @@ class DirectorsController < ApplicationController
     @the_director =  matching_records.at(0)
     render({  :template => "director_templates/details"})
   end
+
+  def youngest
+
+    the_youngest = Director.where.not({ :dob => nil}).order({ :dob => :desc})
+    @youngest = the_youngest.at(0);
+    dob =   @youngest.dob
+    @date_formatted = dob.strftime("%B %d, %Y")
+    render({  :template => "director_templates/youngest"})
+
+  end
+
+  def eldest
+
+    the_eldest = Director.where.not({ :dob => nil}).order({ :dob => :asc})
+    @eldest = the_eldest.at(0);
+    dob =   @eldest.dob
+    @date_formatted = dob.strftime("%B %d, %Y")
+    render({  :template => "director_templates/eldest"})
+
+  end
   
 end
